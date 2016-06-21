646e9da1bc5c505c815075d3943709adfdb639d7
Litto Chacko <littochackomp@gmail.com>
2016-06-21 17:44:30 +0400

    Update Productot.php

--- libs/Productot.php
+++ libs/Productot.php
@@ -1,5 +1,13 @@
 <?php
 class Productot{
+/*
+ * @category  Database Access
+ * @package   MysqliDb
+ * @author    Litto chacko <littochackomp@gmail.com>
+ * @copyright Copyright (c) 2010-2016
+ * @link      https://github.com/litto/PHP-MYSQLI-Framework/ 
+ * @version   2.0-master
+ */
 
  function getmyall($customer,$userid){
 global $db;
