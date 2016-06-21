c8060f1f799c61faa2302e4a14704bbf3f6de287
Litto Chacko <littochackomp@gmail.com>
2016-06-21 17:42:14 +0400

    Update Employeetemp.php

--- libs/Employeetemp.php
+++ libs/Employeetemp.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * @category  Database Access
+ * @package   MysqliDb
+ * @author    Litto chacko <littochackomp@gmail.com>
+ * @copyright Copyright (c) 2010-2016
+ * @link      https://github.com/litto/PHP-MYSQLI-Framework/ 
+ * @version   2.0-master
+ */
 class Employeetemp{
 
  function listall($inputs){
