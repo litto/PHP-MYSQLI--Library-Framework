9a5764599091395f704b447bb2ef0c928881e089
Litto Chacko <littochackomp@gmail.com>
2016-06-21 17:50:14 +0400

    Update add.php

--- add.php
+++ add.php
@@ -1,5 +1,13 @@
 <?php
 include("db.php");
+/*
+ * @category  Database Access
+ * @package   MysqliDb
+ * @author    Litto chacko <littochackomp@gmail.com>
+ * @copyright Copyright (c) 2010-2016
+ * @link      https://github.com/litto/PHP-MYSQLI-Framework/ 
+ * @version   2.0-master
+ */
 
 if(isset($_POST['submit'])){
 
